4968409d6172b51564f19e6f2a1f10ed73d81293
sota4280 <cod413@icloud.com>
2025-08-18 14:34:16 +0900

    added comment

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\Admin\CategoriesController;
 
 Auth::routes();
 
-Route::group(["middleware"=> "auth"], function () {
+Route::group(["middleware"=> "auth"], function () {//17
     Route::get('/',[HomeController::class,'index'])->name("index");//Akane
     Route::get('/people',[HomeController::class,'search'])->name("search");
 
